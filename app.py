4b8f7ff26bee7254d09ef743083fbbc60adaddbe
isaac <isaactanpy2000@gmail.com>
2021-05-31 11:58:58 +0800

    change procfile to Procfile

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def login():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(   environ.get("PORT", 5000)))
 
 #
 # # 2. add records
