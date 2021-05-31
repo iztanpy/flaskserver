82bbfa79c939bbdecd2a0f4724cd0179dbb48851
isaac <isaactanpy2000@gmail.com>
2021-05-31 15:38:40 +0800

    temp

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-db = SQLAlchemy(app)
-
 # connect database to flask app
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/facialdetection'
