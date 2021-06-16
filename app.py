ed80c15b52eec6ea3e961d9fda2b94eab38efc05
isaac <isaactanpy2000@gmail.com>
2021-06-16 15:28:56 +0800

    recording

--- app.py
+++ app.py
@@ -3,15 +3,24 @@ from flask import Flask, request, render_template, make_response, redirect, url_
 from flask_cors import CORS, cross_origin
 import os
 import psycopg2
+import sys
+import base64
+import cv2
+import io
+from imageio import imread
+from PIL import Image
+import numpy as np
+import dlib
+from scipy.spatial import distance
+import time
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+#set up connection to the databse
 DATABASE_URL = os.environ['DATABASE_URL']
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL?sslmode=require')
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-
 uri = os.getenv("DATABASE_URL")  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
@@ -19,8 +28,45 @@ if uri.startswith("postgres://"):
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 db = SQLAlchemy(app)
 
+#set up the functions used for facial detection
+hog_face_detector = dlib.get_frontal_face_detector()
+
+dlib_facelandmark = dlib.shape_predictor(
+    'shape_predictor_68_face_landmarks.dat')
+
+
+ear_collection = []
+threshold = 0.32
+
+def readb64(base64_string):
+    sbuf = io.BytesIO()
+    sbuf.write(base64.b64decode(base64_string))
+    pimg = Image.open(sbuf)
+    return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
+
+def calculate_ear(eye):
+    a = distance.euclidean(eye[1], eye[5])
+    b = distance.euclidean(eye[2], eye[4])
+    c = distance.euclidean(eye[0], eye[3])
+    ear = (a + b) / (2 * c)
+    return ear
+
+
+def mean(lst):
+    if len(lst) == 0:
+        return 0
+    final = 0
+    for i in lst:
+        final += i
+    return final / len(lst)
+
+
+
+
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/facialdetection'
 
+# consumer.py for rabbitmq
 
 # create a user class
 
@@ -37,7 +83,6 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-db.create_all()
 
 @app.route('/')
 def base():
@@ -82,36 +127,58 @@ def login():
 #
 # def calibration():
 
+@app.route('/video_player', methods=['POST'])
+def player():
+    global ear_collection
+    string = request.json.get('picture')
+    img = readb64(string['base64'])
+    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    middle_time = time.time()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = hog_face_detector(gray)
+    avg_ear = 0
+    if not faces:
+        return 'no'
+
+    for face in faces:
+
+        face_landmarks = dlib_facelandmark(gray, face)
+
+        left_eye = []
+
+        right_eye = []
+
+        for point in range(36, 42):
+            x = face_landmarks.part(point).x
+            y = face_landmarks.part(point).y
+            left_eye.append((x, y))
+
+        # maybe to make it faster we can do just one eye? unless the guy wink alot it should be the same
+        for point in range(42, 48):
+            x = face_landmarks.part(point).x
+            y = face_landmarks.part(point).y
+            right_eye.append((x, y))
+
+        left_ear = calculate_ear(left_eye)
+
+        right_ear = calculate_ear(right_eye)
+        avg_ear = (left_ear + right_ear) / 2
+        ear_collection.append(avg_ear)
+
+    ear_collection = ear_collection[-10:]
+    value = mean(ear_collection)
+
+    print(avg_ear)
+    print(len(ear_collection))
+    print(value)
+
+    if value > threshold:
+        return 'no'
+    return 'yes'
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
-#
-# # 2. add records
-# usr1 = User()
-# usr1.id = 1
-# usr1.username = "wang"
-# usr1.password = "wangwang"
-#
-# usr2 = User(id=2, username="yang", password="yang")
-# db.session.add(usr1)
-# print("Add usr1")
-# db.session.add(usr2)
-# print("Add usr2")
-# db.session.commit()
-#
-# # 3. query the record, note that the query returns the object ， if the query does not return none
-# users1 = User.query.all()  #  query all
-# print(users1)
-# print("User Count：", len(users1))
-#
-#
-# user = User.query.get(1)
-# db.session.delete(user)
-# print("Delete usr1")
-# db.session.commit()
-#
-# users2 = User.query.all()  #  query all
-# print(users2)
-# print("User Count：", len(users2))
